c1f58320efa9e0924e1de972005aa7c02bc8e44f
Grey <81944598+Grey03@users.noreply.github.com>
2022-03-01 14:15:24 +0000

    Better stufffs

--- main.py
+++ main.py
@@ -6,62 +6,80 @@
 from PIL import Image
 import math
 
-def ImToTxt(*args):
-  try:
-    if args[0] == int:
-      contrast = args[0]
-      if contrast > 255 or contrast < 0:
-        contrast = 255
-    else:
-      contrast = 128
-  except:
-    contrast = 128
-     
-  name = input("Enter Image Name\nExample (test.jpg)\n: ")
-  
-  print ("\nConverting Name...")
-  img = Image.open(name).convert("L")
-
-  name = name.replace(".", " ")
-  name = name.split()
-  name = name[0]
-  name = (name + "_ToTxt" + ".jpg")
-  
-  img.save(name)
-
-  img = Image.open(name)
-  
-  print ("Resizing Image...")
-  
-  while img.size[0] * img.size[1] > 1000000:
-    img = img.resize((math.ceil(img.size[0]/2),math.ceil(img.size[1]/2)))
+def ImToTxt():
+  def contrast():
+    try:
+      c = int(input("Enter the contrast youd like (1-255): "))
+    except:
+      while c != int():
+        try:
+          c = int(input("Enter the contrast you would like (1-255): "))
+        except:
+          print ("Thats not accepted!")
+    if c > 255 or c < 1:
+      c = 128
+    return c
     
-  while img.size[0]%2 != 0:
-    img = img.resize((img.size[0]-1, img.size[1]))
-  while img.size[1]%4 != 0:
-    img = img.resize((img.size[0], img.size[1]-1))
-
-  img.save(name)
-
-  temp = []
-  print ("Generating Text...")
-  Material1 = ("⬜")
-  Material2 = ("⬛")
-  for y in range(img.size[1]):
-    temp2 = []
-    temp.append(temp2)
-    for x in range(img.size[0]):
-      if ((img.getpixel((x,y))) > contrast):
-        temp2.append(Material1)
-      else:
-        temp2.append(Material2)
-        
-  print ("Writing to output.txt...")
   
-  with open("output.txt", "w") as f:
-    for y in range(len(temp)):
-      f.write("\n")
-      for x in range(len(temp[0])):
-        f.write(str(temp[y][x]))
-  temp = []
+  def nameconverter(name):
+    print ("Converting Name...")
+    name = name.replace(".", " ")
+    name = name.split()
+    name = name[0]
+    name = (name + "_ToTxt" + ".jpg")
+    print ("Name Converted...")
+    return name
+  
+  def imgresizer(img):
+    print ("Resizing Image...")
+    while img.size[0] * img.size[1] > 1000000:
+      img = img.resize((math.ceil(img.size[0]/2),math.ceil(img.size[1]/2)))
+    
+    while img.size[0]%2 != 0:
+      img = img.resize((img.size[0]-1, img.size[1]))
+    while img.size[1]%4 != 0:
+      img = img.resize((img.size[0], img.size[1]-1))
+    print ("Image resized...")
+    return img
+  
+  def createarrays(img,contrastsetting):
+    print ("Generating Text...")
+    Material1 = ("⬜")
+    Material2 = ("⬛")
+    temp = []
+    for y in range(img.size[1]):
+      temp2 = []
+      temp.append(temp2)
+      for x in range(img.size[0]):
+        if ((img.getpixel((x,y))) > contrastsetting):
+          temp2.append(Material1)
+        else:
+          temp2.append(Material2)
+    temp2 = []
+    print ("Text Generated...")
+    return temp
+  
+  def sendtofile(temp):
+    print ("Writing to output.txt...")
+    with open("output.txt", "w") as f:
+      for y in range(len(temp)):
+        f.write("\n")
+        for x in range(len(temp[0])):
+          f.write(str(temp[y][x]))
+    print ("Wrote to output.txt...")
+
+
+
+
+  name = input("Enter Image Name\nExample (test.png)\n: ")
+  print ("")
+  img = Image.open(name).convert("L")
+  name = nameconverter(name)
+  img.save(name)
+  img = Image.open(name)
+  img = imgresizer(img)
+  img.save(name)
+  contrastsetting = contrast()
+  temp = createarrays(img,contrastsetting)
+  sendtofile(temp)        
   print ("Done!")
