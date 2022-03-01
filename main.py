ed05bdfdd6cbc21232b71b7baa3340f8f91ac651
Grey <81944598+Grey03@users.noreply.github.com>
2022-03-01 15:38:21 +0000

    Fixed a few bugs added a few features such as changing materials and inverting them as well as custom diameters.

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ def ImToTxt():
     if c > 255 or c < 1:
       c = 128
     return c
-    
   
   def nameconverter(name):
     print ("Converting Name...")
     name = name.replace(".", " ")
     name = name.split()
     name = name[0]
-    name = (name + "_ToTxt" + ".jpg")
+    name = ("TEMPIMAGENOTOUCH" + ".jpg")
     print ("Name Converted...")
     return name
   
@@ -34,18 +33,24 @@ def ImToTxt():
     print ("Resizing Image...")
     while img.size[0] * img.size[1] > 1000000:
       img = img.resize((math.ceil(img.size[0]/2),math.ceil(img.size[1]/2)))
-    
-    while img.size[0]%2 != 0:
-      img = img.resize((img.size[0]-1, img.size[1]))
-    while img.size[1]%4 != 0:
-      img = img.resize((img.size[0], img.size[1]-1))
+    #Only use in braile version
+    #while img.size[0]%2 != 0:
+      #img = img.resize((img.size[0]-1, img.size[1]))
+    #while img.size[1]%4 != 0:
+      #img = img.resize((img.size[0], img.size[1]-1))
     print ("Image resized...")
     return img
   
-  def createarrays(img,contrastsetting):
+  def createarrays(img,contrastsetting,mats):
     print ("Generating Text...")
-    Material1 = ("⬜")
-    Material2 = ("⬛")
+    inverse = input("Would you like to inverse the color scheme y/n : ").lower()
+    if inverse == "y":
+      Material1 = mats[0]
+      Material2 = mats[1]
+    else:
+      Material1 = mats[1]
+      Material2 = mats[0]
+      
     temp = []
     for y in range(img.size[1]):
       temp2 = []
@@ -63,23 +68,35 @@ def ImToTxt():
     print ("Writing to output.txt...")
     with open("output.txt", "w") as f:
       for y in range(len(temp)):
-        f.write("\n")
+        if y != 0:
+          f.write("\n")
         for x in range(len(temp[0])):
           f.write(str(temp[y][x]))
     print ("Wrote to output.txt...")
 
-
-
-
   name = input("Enter Image Name\nExample (test.png)\n: ")
   print ("")
+  dia = input("Would you like to set a diameter? y/n : ").lower() 
   img = Image.open(name).convert("L")
   name = nameconverter(name)
   img.save(name)
   img = Image.open(name)
+  if dia == "y":
+    newx = int(input("Enter the x size: "))
+    newy = int(input("Enter the y size: "))
+    img = img.resize((newx,newy))
   img = imgresizer(img)
   img.save(name)
   contrastsetting = contrast()
-  temp = createarrays(img,contrastsetting)
+  changemat = input("Change materials? y/n : ").lower()
+  if changemat == "y":
+    mat1 = input("Enter the first material: ")
+    mat2 = input("Enter the second material: ")
+    mats = [mat1,mat2]    
+  else:
+    mats = ["⬛","⬜"]
+  temp = createarrays(img,contrastsetting,mats)
   sendtofile(temp)        
   print ("Done!")
+  
+ImToTxt()
