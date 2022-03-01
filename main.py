105d16ca23a08abff35d6e1402a2dca5902751f8
Grey <81944598+Grey03@users.noreply.github.com>
2022-03-01 13:38:18 +0000

    Updated a few things before I work on braille

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Should look at an image
 #http://xahlee.info/comp/unicode_braille.html
+#https://home.unicode.org/
 #https://www.tutorialspoint.com/working-with-images-in-python
 #https://www.geeksforgeeks.org/python-pil-getpixel-method/
 from PIL import Image
@@ -15,12 +16,12 @@ def ImToTxt(*args):
       contrast = 128
   except:
     contrast = 128
-    
+     
   name = input("Enter Image Name\nExample (test.jpg)\n: ")
-
+  
   print ("\nConverting Name...")
   img = Image.open(name).convert("L")
-  
+
   name = name.replace(".", " ")
   name = name.split()
   name = name[0]
@@ -29,27 +30,34 @@ def ImToTxt(*args):
   img.save(name)
 
   img = Image.open(name)
+  
   print ("Resizing Image...")
-  while img.size[0]%2 != 0:
-    img = img.resize((img.size[0]-1, img.size[1]))
-  while img.size[1]%8 != 0:
-    img = img.resize((img.size[0], img.size[1]-1))
-
+  
   while img.size[0] * img.size[1] > 1000000:
     img = img.resize((math.ceil(img.size[0]/2),math.ceil(img.size[1]/2)))
+    
+  while img.size[0]%2 != 0:
+    img = img.resize((img.size[0]-1, img.size[1]))
+  while img.size[1]%4 != 0:
+    img = img.resize((img.size[0], img.size[1]-1))
+
   img.save(name)
 
   temp = []
   print ("Generating Text...")
+  Material1 = ("⬜")
+  Material2 = ("⬛")
   for y in range(img.size[1]):
     temp2 = []
     temp.append(temp2)
     for x in range(img.size[0]):
       if ((img.getpixel((x,y))) > contrast):
-        temp2.append("•")
+        temp2.append(Material1)
       else:
-        temp2.append(" ")
+        temp2.append(Material2)
+        
   print ("Writing to output.txt...")
+  
   with open("output.txt", "w") as f:
     for y in range(len(temp)):
       f.write("\n")
